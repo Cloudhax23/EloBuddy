3b4c709e40f8d586165b38b0b530b39d3ed6ac3c
Cloudhax23 <qadeem@aplabs.us>
2015-09-22 20:40:01 -0500

    first commit

---
+++ Graves/Graves/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EloBuddy;
+using EloBuddy.SDK;
+using EloBuddy.SDK.Enumerations;
+using EloBuddy.SDK.Events;
+using EloBuddy.SDK.Menu;
+using EloBuddy.SDK.Menu.Values;
+
+
+namespace Graves
+{
+    class Program
+    {
+        public static Spell.Skillshot Q;
+        public static Spell.Skillshot W;
+        public static Spell.Skillshot E;
+        public static Spell.Skillshot R;
+
+        static void Main(string[] args)
+        {
+            Loading.OnLoadingComplete += Loading_OnLoadingComplete;
+        }
+        public static AIHeroClient _Player
+        {
+            get { return ObjectManager.Player; }
+        }
+
+        public static Menu menu, ComboMenu;
+
+        private static void Loading_OnLoadingComplete(EventArgs args)
+        {
+            Bootstrap.Init(null);
+
+            Q = new Spell.Skillshot(SpellSlot.Q, (int)720f, SkillShotType.Linear, (int)0.25f, (int)2000f, (int)(15f * (float)Math.PI / 180));
+            W = new Spell.Skillshot(SpellSlot.W, (int)850f, SkillShotType.Circular, (int)0.25f, (int)1650f, (int)250f);
+            E = new Spell.Skillshot(SpellSlot.E, (uint)_Player.GetAutoAttackRange(), SkillShotType.Circular); // Fluxy Vayne Q modify
+            R = new Spell.Skillshot(SpellSlot.R, (int)1100f, SkillShotType.Linear, (int)0.25f, (int)2100f, (int)100f);
+
+            menu = MainMenu.AddMenu("Graves", "Graves");
+            menu.AddGroupLabel("First plugin I made! Credits to VnHarry and Fluxy");
+
+            ComboMenu = menu.AddSubMenu("Combo", "Combo");
+            ComboMenu.AddGroupLabel("Combo Settings");
+            ComboMenu.AddSeparator();
+            ComboMenu.Add("useQCombo", new CheckBox("Use Q"));
+            ComboMenu.Add("useWCombo", new CheckBox("Use W"));
+            ComboMenu.Add("useECombo", new CheckBox("Use E"));
+            ComboMenu.Add("useRCombo", new CheckBox("Use R"));
+
+            Game.OnTick += Game_OnTick;
+        }
+
+        private static void Game_OnTick(EventArgs args)
+        {
+            Orbwalker.ForcedTarget = null;
+            if (Orbwalker.ActiveModesFlags.HasFlag(Orbwalker.ActiveModes.Combo)) Combo();
+        }
+
+        public static float RDamage(Obj_AI_Base target)
+        {
+            return _Player.CalculateDamageOnUnit(target, DamageType.Physical,
+                (float)(new[] { 250, 400, 550 }[Program.R.Level] + 1.5 * _Player.FlatPhysicalDamageMod));
+        }
+        public static void Combo()
+        {
+            var useQ = ComboMenu["useQCombo"].Cast<CheckBox>().CurrentValue;
+            var useW = ComboMenu["useWCombo"].Cast<CheckBox>().CurrentValue;
+            var useE = ComboMenu["useECombo"].Cast<CheckBox>().CurrentValue;
+            var useR = ComboMenu["useRCombo"].Cast<CheckBox>().CurrentValue;
+
+            foreach (var target in HeroManager.Enemies.Where(o => o.IsValidTarget(1200) && !o.IsDead && !o.IsZombie))
+            {
+                if (useQ && Q.IsReady() && Q.GetPrediction(target).HitChance >= HitChance.Medium)
+                {
+                    Q.Cast(target);
+                }
+                if (useW && W.IsReady() && W.GetPrediction(target).HitChance >= HitChance.Medium && target.IsValidTarget(W.Range))
+                {
+                    W.Cast(target);
+                }
+                if (useE && E.IsReady() && target.IsValidTarget(700))
+                {
+                    E.Cast(Game.CursorPos);
+                }
+                if (useR && R.IsReady() && R.GetPrediction(target).HitChance >= HitChance.High && target.Health <= RDamage(target) && target.IsValidTarget(R.Range))
+                {
+                    R.Cast(target);
+                }
+            }
+        }
+    }
+}
