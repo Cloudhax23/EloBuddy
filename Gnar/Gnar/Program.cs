7e6baaa62d945bba757d2bfffff9ce40f893b5f3
Cloudhax23 <qadeem@aplabs.us>
2015-10-13 07:44:01 -0500

    Update on QE pred for big gnar!

--- Gnar/Gnar/Program.cs
+++ Gnar/Gnar/Program.cs
@@ -9,6 +9,8 @@ using EloBuddy.SDK.Enumerations;
 using EloBuddy.SDK.Events;
 using EloBuddy.SDK.Menu;
 using EloBuddy.SDK.Menu.Values;
+using EloBuddy.SDK.Rendering;
+using SharpDX;
 
 
 namespace Gnar
@@ -30,7 +32,20 @@ namespace Gnar
         {
             get { return ObjectManager.Player; }
         }
-        
+
+        public static string Model = ObjectManager.Player.BaseSkinName; 
+
+        public static bool IsMiniGnar()
+        {
+            return _Player.BaseSkinName == "Gnar";
+        }
+
+        public static bool IsMegaGnar()
+        {
+            return _Player.BaseSkinName == "gnarbig";
+        }
+
+        private static Vector3 mousePos { get { return Game.CursorPos; } }
 
         public static Menu menu, ComboMenu, HarassMenu, LastHitMenu, LaneClearMenu, MiscMenu;
 
@@ -38,9 +53,11 @@ namespace Gnar
         {
             Bootstrap.Init(null);
 
-            Q = new Spell.Skillshot(SpellSlot.Q, 1125, SkillShotType.Linear,250,2500,60);
+            Q = new Spell.Skillshot(SpellSlot.Q, 1125, SkillShotType.Linear,250,1200,60);
+            Q2 = new Spell.Skillshot(SpellSlot.Q, 1125, SkillShotType.Linear, 250, 1200, 80);
             WB = new Spell.Skillshot(SpellSlot.W, 475, SkillShotType.Linear);
-            E = new Spell.Skillshot(SpellSlot.E, 475, SkillShotType.Circular,0,1000,200);
+            E = new Spell.Skillshot(SpellSlot.E, 475, SkillShotType.Circular,50,1000,150);
+            E2 = new Spell.Skillshot(SpellSlot.E, 475, SkillShotType.Circular, 50, 1000, 200);
             R = new Spell.Skillshot(SpellSlot.R, 490,SkillShotType.Circular);
 
             menu = MainMenu.AddMenu("Gnar", "Gnar");
@@ -49,7 +66,9 @@ namespace Gnar
             ComboMenu.AddGroupLabel("Combo Settings");
             ComboMenu.AddSeparator();
             ComboMenu.Add("useQCombo", new CheckBox("Use Q"));
+            ComboMenu.Add("useQ2Combo", new CheckBox("Use Q Big"));
             ComboMenu.Add("useECombo", new CheckBox("Use E"));
+            ComboMenu.Add("useE2Combo", new CheckBox("Use E Big"));
             ComboMenu.Add("useWBCombo", new CheckBox("Use W"));
             ComboMenu.Add("useRCombo", new CheckBox("Use R"));
             ComboMenu.Add("useRComboSlider", new Slider("Subtract Ult Push by: ", 20, 0, 100));
@@ -75,6 +94,8 @@ namespace Gnar
             MiscMenu.AddGroupLabel("Misc Settings");
             MiscMenu.AddSeparator();
             MiscMenu.Add("useR", new CheckBox("Auto R"));
+            MiscMenu.Add("useW", new CheckBox("Auto W to Interrupt"));
+            MiscMenu.Add("useQW", new CheckBox("Auto Q if enemy has 2 stacks of W"));
             MiscMenu.Add("R", new Slider("When X enemy is stunnable by R", 2, 5, 0));
 
             Game.OnTick += Game_OnTick;
@@ -95,6 +116,7 @@ namespace Gnar
             if (_Player.CountEnemiesInRange(R.Range) >= Ult.getSliderValue(Program.MiscMenu, "R")) 
              {
                  var useR = MiscMenu["useR"].Cast<CheckBox>().CurrentValue;
+                 var useQ = MiscMenu["useQW"].Cast<CheckBox>().CurrentValue;
                  if (useR && R.IsReady())
                  {
                      AIHeroClient priorityTarget = null;
@@ -115,28 +137,55 @@ namespace Gnar
                          R.Cast(priorityTarget);
                      }
                  }
+                 foreach (var target in HeroManager.Enemies.Where(o => o.IsValidTarget(1400) && !o.IsDead && !o.IsZombie))
+                 {
+                     if (useQ && Q.IsReady() && Q.GetPrediction(target).HitChance >= HitChance.Medium && target.GetBuffCount("gnarwproc") == 2)
+                     {
+                         Q.Cast(target);
+                     }
+                 }
              }
         }
+
+         static void OnInterruptableSpell(Obj_AI_Base sender, Interrupter.InterruptableSpellEventArgs args)
+         {
+             var useW = MiscMenu["useW"].Cast<CheckBox>().CurrentValue;
+             if (useW && sender.IsValidTarget(WB.Range))
+             {
+                 WB.Cast(args.Sender);
+             }
+         }
+
         public static void Combo()
         {
             var useQ = ComboMenu["useQCombo"].Cast<CheckBox>().CurrentValue;
+            var useQ2 = ComboMenu["useQ2Combo"].Cast<CheckBox>().CurrentValue;
             var useW = ComboMenu["useWBCombo"].Cast<CheckBox>().CurrentValue;
             var useE = ComboMenu["useECombo"].Cast<CheckBox>().CurrentValue;
+            var useE2 = ComboMenu["useE2Combo"].Cast<CheckBox>().CurrentValue;
             var useR = ComboMenu["useRCombo"].Cast<CheckBox>().CurrentValue;
 
             foreach (var target in HeroManager.Enemies.Where(o => o.IsValidTarget(1400) && !o.IsDead && !o.IsZombie))
             {
-                if (useQ && Q.IsReady() && Q.GetPrediction(target).HitChance >= HitChance.Medium)
+                if (useQ2 && Q2.IsReady() && IsMegaGnar() && Q2.GetPrediction(target).HitChance >= HitChance.Medium)
+                {
+                    Q2.Cast(target);
+                }
+                if (useQ && Q.IsReady() && IsMiniGnar() && Q.GetPrediction(target).HitChance >= HitChance.Medium)
                 {
                     Q.Cast(target);
                 }
-                if (useW && WB.IsReady() && WB.GetPrediction(target).HitChance >= HitChance.Medium && target.IsValidTarget(WB.Range) && target.HasBuffOfType(BuffType.Stun))
+                if (useW && WB.IsReady() && WB.GetPrediction(target).HitChance >= HitChance.Medium && target.IsValidTarget(WB.Range) && !target.HasBuffOfType(BuffType.Stun))
                 {
                     WB.Cast(target);
                 }
-                if (useE && E.IsReady() && target.IsValidTarget(700))
+                if (useE && E.IsReady() && target.IsValidTarget(700) && IsMiniGnar())
                 {
-                    E.Cast(Game.CursorPos);
+                    E.Cast(mousePos);
+                }
+                if (useE2 && E2.IsReady() && target.IsValidTarget(700) && IsMegaGnar())
+                {
+                    E2.Cast(mousePos);
                 }
                 if (useR && R.IsReady() && _Player.CountEnemiesInRange(R.Range) >= Ult.getSliderValue(Program.MiscMenu, "R"))
             {
@@ -194,7 +243,7 @@ namespace Gnar
             {
                 Q.Cast(minions);
             }
-            if (useW && !minions.IsDead && WB.GetPrediction(minions).HitChance >= HitChance.Medium)
+            if (useW && !minions.IsDead && Q.GetPrediction(minions).HitChance >= HitChance.Medium)
             {
                 WB.Cast(minions);
             }
